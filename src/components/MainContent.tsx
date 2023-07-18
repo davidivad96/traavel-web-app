c40c35bca3c9135f368245f354c4afc96d103c4c
davidivad96 <david@strivelabs.io>
2023-07-18 16:37:40 +0200

    improve styling of the expander of activities; add edit and delete buttons to each activity (functionality to be implemented)

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -11,8 +11,10 @@ import {
 } from "@aws-amplify/ui-react";
 import { FaEdit } from "react-icons/fa";
 import { AiOutlinePlus } from "react-icons/ai";
+import { MdEdit, MdDelete } from "react-icons/md";
 import dayjs from "dayjs";
 import { Activity } from "@/models";
+import { IconButton } from "@mui/material";
 
 interface Props {
   title?: string;
@@ -30,69 +32,117 @@ export const MainContent = ({
   openEditImageModal,
   handleOnClickNewActivity,
   activities,
-}: Props) => (
-  <Flex direction="column" flex={1}>
-    <View
-      width="100%"
-      minHeight={220}
-      position="relative"
-      onClick={openEditImageModal}
-      style={{ cursor: "pointer" }}
-    >
-      <Image
-        src={imgUrl || "/images/default_trip_image.png"}
-        alt="Trip image"
-        fill
-        objectFit="cover"
-      />
-      <Flex id="edit-image-button" justifyContent="center" alignItems="center">
-        <FaEdit width="100%" color="#FFF" />
-      </Flex>
-    </View>
-    {title ? (
+}: Props) => {
+  const handleOnClickEditActivityButton = (
+    e: React.MouseEvent<HTMLElement>
+  ) => {
+    e.stopPropagation();
+  };
+
+  const handleOnClickDeleteActivityButton = (
+    e: React.MouseEvent<HTMLElement>
+  ) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <Flex direction="column" flex={1}>
       <View
-        padding="0 30px 20px 30px"
-        height="calc(100vh - 70px)"
-        style={{ overflowY: "scroll" }}
+        width="100%"
+        minHeight={220}
+        position="relative"
+        onClick={openEditImageModal}
+        style={{ cursor: "pointer" }}
       >
+        <Image
+          src={imgUrl || "/images/default_trip_image.png"}
+          alt="Trip image"
+          fill
+          objectFit="cover"
+        />
         <Flex
-          direction="row"
-          justifyContent="space-between"
+          id="edit-image-button"
+          justifyContent="center"
           alignItems="center"
-          margin="10px 0"
         >
-          <Heading level={4}>{title}</Heading>
-          <Button variation="link" onClick={handleOnClickNewActivity}>
-            <AiOutlinePlus size={22} style={{ marginRight: "5px" }} /> New
-            activity
-          </Button>
+          <FaEdit width="100%" color="#FFF" />
         </Flex>
-        {isLoadingActivities ? (
-          <Flex justifyContent="center" marginTop={20}>
-            <Loader width={50} height={50} />
-          </Flex>
-        ) : (
-          <Expander type="single">
-            {activities.map((activity, index) => (
-              <ExpanderItem
-                key={`${activity.dayId}-${index}`}
-                title={
-                  <Text>
-                    {dayjs(new Date(activity.startTime)).format("HH:mm")} -{" "}
-                    {dayjs(new Date(activity.endTime)).format("HH:mm")}
-                  </Text>
-                }
-                value={`${activity.dayId}-${index}`}
-              >
-                <Text>{activity.name}</Text>
-                <Text>{activity.description}</Text>
-              </ExpanderItem>
-            ))}
-          </Expander>
-        )}
       </View>
-    ) : (
-      <Text textAlign="center">Select a date on the sidebar</Text>
-    )}
-  </Flex>
-);
+      {title ? (
+        <View
+          padding="0 30px 20px 30px"
+          height="calc(100vh - 70px)"
+          style={{ overflowY: "scroll" }}
+        >
+          <Flex
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            margin="10px 0"
+          >
+            <Heading level={4}>{title}</Heading>
+            <Button variation="link" onClick={handleOnClickNewActivity}>
+              <AiOutlinePlus size={22} style={{ marginRight: "5px" }} /> New
+              activity
+            </Button>
+          </Flex>
+          {isLoadingActivities ? (
+            <Flex justifyContent="center" marginTop={20}>
+              <Loader width={50} height={50} />
+            </Flex>
+          ) : (
+            <Expander id="activity-expander" type="single">
+              {activities.map((activity, index) => (
+                <ExpanderItem
+                  key={`${activity.dayId}-${index}`}
+                  title={
+                    <Flex
+                      direction="row"
+                      flex={1}
+                      justifyContent="space-between"
+                      alignItems="center"
+                    >
+                      <Flex direction="row">
+                        <Text>
+                          {dayjs(new Date(activity.startTime)).format("HH:mm")}{" "}
+                          - {dayjs(new Date(activity.endTime)).format("HH:mm")}:{" "}
+                        </Text>
+                        <Text fontWeight="bold">{activity.name}</Text>
+                      </Flex>
+                      <Flex direction="row" style={{ gap: 0 }}>
+                        <IconButton
+                          size="small"
+                          style={{ color: "#367B92" }}
+                          onClick={handleOnClickEditActivityButton}
+                        >
+                          <MdEdit />
+                        </IconButton>
+                        <IconButton
+                          size="small"
+                          style={{ color: "#D65745" }}
+                          onClick={handleOnClickDeleteActivityButton}
+                        >
+                          <MdDelete />
+                        </IconButton>
+                      </Flex>
+                    </Flex>
+                  }
+                  value={`${activity.dayId}-${index}`}
+                  style={{ cursor: "pointer" }}
+                >
+                  {activity.description ? (
+                    <Text>{activity.description}</Text>
+                  ) : (
+                    <Text fontStyle="italic">No description provided</Text>
+                  )}
+                </ExpanderItem>
+              ))}
+            </Expander>
+          )}
+        </View>
+      ) : (
+        <Text textAlign="center">Select a date on the sidebar</Text>
+      )}
+    </Flex>
+  );
+};
